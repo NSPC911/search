a4968b73d2c3abe6e248d3a63dd4b88f9dabf14e
NSPC911 <notsoprocoder911@gmail.com>
2024-08-31 11:23:14 +0800

    fixed my brain
    not sure what made me create code like this

--- config.py
+++ config.py
@@ -61,7 +61,8 @@ def configure(listarg):
         print(f"{Fore.RED}FlagError: Expected modifier keyword after `--config` but received None")
         exit(1)
     try:
-        listarg[2]
+        if listarg[2] in ["reset","update"]:
+            raise IndexError
     except IndexError:
         if listarg[1] == "update":
             response = requests.get(url)
@@ -84,7 +85,11 @@ def configure(listarg):
                 print(f"{Fore.RED}RequestError: Couldn't fetch data from {url}")
                 exit(1)
             return
-        elif listarg[1] != "list":
+        elif listarg[1] == "list":
+            print(f"{Fore.GREEN}Listing all keys in search.config.json:")
+            for key in load_json(config_path):
+                print(f"{Fore.CYAN}{key}{Fore.WHITE} is set as {Fore.MAGENTA}{config('read',key)}")
+        else:
             print(f"{Fore.RED}FlagError: Expected key to `{listarg[1]}` but received None")
             exit(1)
     try:
@@ -101,29 +106,23 @@ def configure(listarg):
                 else:
                     config("write",listarg[2],listarg[3].lower())
         elif listarg[1] == "read":
-            raise IndexError
-        elif listarg[1] == "list":
-            raise IndexError
+            print(f"`{Fore.CYAN}{listarg[2]}{Fore.WHITE}` is set as {Fore.MAGENTA}{config('read',listarg[2])}")
+            if listarg[2].startswith("clr") and listarg[2].endswith("foreground"):
+                print(f"Allowed definitions:\n{Fore.RED}red, {Fore.GREEN}green, {Fore.YELLOW}yellow, {Fore.BLUE}blue, {Fore.MAGENTA}magenta, {Fore.CYAN}cyan, {Fore.WHITE}white, {Fore.BLACK}black\n{Fore.LIGHTBLACK_EX}lightblack_ex, {Fore.LIGHTBLUE_EX}lightblue_ex, {Fore.LIGHTCYAN_EX}lightcyan_ex, {Fore.LIGHTGREEN_EX}lightgreen_ex\n{Fore.LIGHTMAGENTA_EX}lightmagenta_ex, {Fore.LIGHTRED_EX}lightred_ex, {Fore.LIGHTWHITE_EX}lightwhite_ex, {Fore.LIGHTYELLOW_EX}lightyellow_ex{reset()}\nYou cannot see some colors as your terminal's background color is set that way.")
+            elif listarg[2].startswith("clr") and listarg[2].endswith("background"):
+                print(f"Allowed definitions:\n{Back.RED}red, {Back.GREEN}green, {Back.YELLOW}yellow, {Back.BLUE}blue, {Back.MAGENTA}magenta, {Back.CYAN}cyan, {Back.WHITE}white, {Back.BLACK}black\n{Back.LIGHTBLACK_EX}lightblack_ex, {Back.LIGHTBLUE_EX}lightblue_ex, {Back.LIGHTCYAN_EX}lightcyan_ex, {Back.LIGHTGREEN_EX}lightgreen_ex\n{Back.LIGHTMAGENTA_EX}lightmagenta_ex, {Back.LIGHTRED_EX}lightred_ex, {Back.LIGHTWHITE_EX}lightwhite_ex, {Back.LIGHTYELLOW_EX}lightyellow_ex{reset()}\nYou cannot see some colors as your terminal's background color is set that way.\n\nThe green color stretching from lightyellow_ex seems to be a bug that I can't fix.\nIf you find a fix, please make a PR.")
+            elif listarg[2].startswith("clr") and listarg[2].endswith("style"):
+                print(f"Allowed definitions: {Style.BRIGHT}BRIGHT, {Style.NORMAL}NORMAL, {Style.DIM}DIM, {Style.RESET_ALL}RESET_ALL")
+            elif listarg[2] in ["default.include_filename","default.in_cwd","first_run","easter_errors"]:
+                print(f"Allowed definitions: {Fore.GREEN}True, {Fore.RED}False")
+            elif listarg[2] == "default.context":
+                print(f"Allowed definitions: Any integer above 0")
     except IndexError:
         if listarg[1] == "set":
             print(f"{Fore.RED}FlagError: Expected value to set to `{listarg[2]}` but received None")
             exit(1)
-        elif listarg[1] == "read":
-            print(f"`{Fore.CYAN}{listarg[2]}{Fore.WHITE}` is set as {Fore.MAGENTA}{config('read',listarg[2])}")
-            if listarg[2].startswith("clr") and (listarg[2].endswith("foreground") or listarg[2].endswith("background")):
-                print(f"Allowed definitions: {Fore.RED}red, {Fore.GREEN}green, {Fore.YELLOW}yellow, {Fore.BLUE}blue, {Fore.MAGENTA}magenta, {Fore.CYAN}cyan, {Fore.WHITE}white, {Fore.BLACK}black")
-            elif listarg[2].startswith("clr") and listarg[2].endswith("style"):
-                print(f"Allowed definitions: {Style.BRIGHT}BRIGHT, {Style.NORMAL}NORMAL, {Style.DIM}DIM, {Style.RESET_ALL}RESET_ALL")
-            elif listarg[2] in ["default.include_filename","default.in_cwd","first_run","easter_errors"]:
-                print(f"Allowed definitions: {Fore.MAGENTA}True, {Fore.MAGENTA}False")
-            elif listarg[2] == "default.context":
-                print(f"Allowed definitions: Any integer above 0")
-        elif listarg[1] == "list":
-            print(f"{Fore.GREEN}Listing all keys in search.config.json:")
-            for key in load_json(config_path):
-                print(f"{Fore.CYAN}{key}{Fore.WHITE} is set as {Fore.MAGENTA}{config('read',key)}")
         else:
-            print(f"{Fore.RED}FlagError: Expected `list`, `read` or `write` after `--config` but received {listarg[1]}")
+            print(f"{Fore.RED}FlagError: Expected `list`, `read`, `write`, `update` or `reset` after `--config` but received {listarg[1]}")
             exit(1)
 
 if f"scoop{os.path.sep}apps" in config_path:
