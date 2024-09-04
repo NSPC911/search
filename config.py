c07822bb029a1f826cb428c0d50571543e4ccbe4
NSPC911 <notsoprocoder911@gmail.com>
2024-09-04 19:56:41 +0800

    made setting config a bit stricter

--- config.py
+++ config.py
@@ -51,9 +51,14 @@ def config(readorset, key, changeto="", is_theme=False):
                 print(f"{Fore.RED}KeyError: `{key}` not found in search.config.json")
                 exit(1)
     elif readorset == "set":
-        cnfg[key] = changeto
-        dump_json("search.config.json",cnfg)
-        print(f"{Fore.GREEN}Set `{Fore.CYAN}{key}{Fore.GREEN}` to {Fore.MAGENTA}{changeto}")
+        try:
+            if cnfg[key]:
+                cnfg[key] = changeto
+            dump_json("search.config.json",cnfg)
+            print(f"{Fore.GREEN}Set `{Fore.CYAN}{key}{Fore.GREEN}` to {Fore.MAGENTA}{changeto}")
+        except KeyError:
+            print(f"{Fore.RED}KeyError: `{key}` not found in search.config.json")
+            exit(1)
 
 
 def reset():
@@ -87,6 +92,7 @@ def configure(listarg):
             print(f"{Fore.GREEN}Listing all keys in search.config.json:")
             for key in load_json(config_path):
                 print(f"{Fore.CYAN}{key}{Fore.WHITE} is set as {Fore.MAGENTA}{config('read',key)}")
+            exit(0)
         elif listarg[0] == "where":
             print(f"{Fore.GREEN}search.config.json is located at {Fore.CYAN}{config_path}")
         else:
@@ -96,10 +102,32 @@ def configure(listarg):
         last = listarg.pop().split()
         listarg.extend(last)
         if listarg[0] == "set":
-            if listarg[2].lower() == "true":
-                config("set",listarg[1],True)
-            elif listarg[2].lower() == "false":
-                config("set",listarg[1],False)
+            if listarg[2].lower() in ["true","false"] and listarg[1] in ["default.include_filename","default.in_cwd","default.case_sensitive"]:
+                config("set",listarg[1],listarg[2].lower()=="true")
+            elif listarg[1] in ["default.include_filename","default.in_cwd","default.case_sensitive"]:
+                print(f"Allowed definitions: {Fore.GREEN}True, {Fore.RED}False")
+                exit(1)
+            elif listarg[1] == "default.context" and listarg[2].isnumeric():
+                config("set",listarg[1],int(listarg[2]))
+            elif listarg[1] == "default.context":
+                print(f"Allowed definitions: Any integer above 0")
+                exit(1)
+            elif listarg[1].startswith("clr") and (listarg[1].endswith("foreground") or listarg[1].endswith("background")) and listarg[2].lower() in ["red","green","yellow","blue","magenta","cyan","white","black","lightblack_ex","lightblue_ex","lightcyan_ex","lightgreen_ex","lightmagenta_ex","lightred_ex","lightwhite_ex","lightyellow_ex"]:
+                config("set",listarg[1],listarg[2].lower())
+            elif listarg[1].startswith("clr") and listarg[1].endswith("foreground"):
+                print(f"Allowed definitions:\n{Fore.RED}red, {Fore.GREEN}green, {Fore.YELLOW}yellow, {Fore.BLUE}blue, {Fore.MAGENTA}magenta, {Fore.CYAN}cyan, {Fore.WHITE}white, {Fore.BLACK}black\n{Fore.LIGHTBLACK_EX}lightblack_ex, {Fore.LIGHTBLUE_EX}lightblue_ex, {Fore.LIGHTCYAN_EX}lightcyan_ex, {Fore.LIGHTGREEN_EX}lightgreen_ex\n{Fore.LIGHTMAGENTA_EX}lightmagenta_ex, {Fore.LIGHTRED_EX}lightred_ex, {Fore.LIGHTWHITE_EX}lightwhite_ex, {Fore.LIGHTYELLOW_EX}lightyellow_ex{reset()}\nYou cannot see some colors as your terminal's background color is set that way.")
+                exit(1)
+            elif listarg[1].startswith("clr") and listarg[1].endswith("background"):
+                print(f"Allowed definitions:\n{Back.RED}red, {Back.GREEN}green, {Back.YELLOW}yellow, {Back.BLUE}blue, {Back.MAGENTA}magenta, {Back.CYAN}cyan, {Back.WHITE}white, {Back.BLACK}black\n{Back.LIGHTBLACK_EX}lightblack_ex, {Back.LIGHTBLUE_EX}lightblue_ex, {Back.LIGHTCYAN_EX}lightcyan_ex, {Back.LIGHTGREEN_EX}lightgreen_ex\n{Back.LIGHTMAGENTA_EX}lightmagenta_ex, {Back.LIGHTRED_EX}lightred_ex, {Back.LIGHTWHITE_EX}lightwhite_ex, {Back.LIGHTYELLOW_EX}lightyellow_ex{reset()}\nYou cannot see some colors as your terminal's background color is set that way.\n\nThe green color stretching from lightyellow_ex seems to be a bug that I can't fix.\nIf you find a fix, please make a PR.")
+                exit(1)
+            elif listarg[1].startswith("clr") and listarg[1].endswith("style") and listarg[2].lower() in ["bright","normal","dim","reset_all"]:
+                config("set",listarg[1],listarg[2].lower())
+            elif listarg[1].startswith("clr") and listarg[1].endswith("style"):
+                print(f"Allowed definitions: {Style.BRIGHT}BRIGHT, {Style.NORMAL}NORMAL, {Style.DIM}DIM, {Style.RESET_ALL}RESET_ALL")
+                exit(1)
+            elif listarg[1] == "current_version":
+                print(f"{Fore.RED}ConfigError: Cannot change `current_version` in search.config.json")
+                exit(1)
             else:
                 if listarg[2].isnumeric():
                     config("set",listarg[1],int(listarg[2]))
@@ -117,6 +145,7 @@ def configure(listarg):
                 print(f"Allowed definitions: {Fore.GREEN}True, {Fore.RED}False")
             elif listarg[1] == "default.context":
                 print(f"Allowed definitions: Any integer above 0")
+            exit(0)
         else:
             print(f"{Fore.RED}FlagError: Expected {Fore.YELLOW}`list`{Fore.RED}, {Fore.YELLOW}`read`{Fore.RED}, {Fore.YELLOW}`set`{Fore.RED} or {Fore.YELLOW}`reset`{Fore.RED} after {Fore.YELLOW}`--config`{Fore.RED} but received {Fore.YELLOW}{listarg[0]}{Fore.RED}")
             exit(1)
