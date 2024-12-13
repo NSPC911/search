7ca710add26e03cee714acc5877c59e0cce21516
NSPC911 <87571998+NSPC911@users.noreply.github.com>
2024-12-13 18:02:26 +0800

    archive creator update
    now properly makes artifacts

--- archive_creator.py
+++ archive_creator.py
@@ -1,7 +1,10 @@
-import zipfile, os, hashlib
+import os, hashlib, argparse
+from zipfile import ZipFile, ZIP_DEFLATED
 from colorama import *
 init(strip=False, convert=False, autoreset=True)
-
+parser = argparse.ArgumentParser(description=None)
+parser.add_argument("--workflow", action="store_true", default=False, help="Enable workflow mode")
+args = parser.parse_args()
 
 root_dir = os.path.dirname(os.path.abspath(__file__))
 archives_dir = os.path.join(root_dir, 'archives')
@@ -20,7 +23,7 @@ archive_specs = {
 
 for archive_name, files_to_include in archive_specs.items():
     archive_path = os.path.join(archives_dir, archive_name)
-    with zipfile.ZipFile(archive_path, 'w', zipfile.ZIP_DEFLATED) as archive:
+    with ZipFile(archive_path, 'w', ZIP_DEFLATED) as archive:
         for file in files_to_include:
             archive.write(os.path.join(root_dir, file), file)
 
@@ -34,5 +37,13 @@ with open(f'{archives_dir}/bash.zip', "rb") as f:
     for byte_block in iter(lambda: f.read(4096), b""):
         hasher.update(byte_block)
     bashhash = hasher.hexdigest()
-print(f"{Fore.YELLOW}SHA256{Fore.WHITE} for {Fore.BLUE}bat.zip{Fore.WHITE}  is {Fore.RED}{bathash}")
-print(f"{Fore.YELLOW}SHA256{Fore.WHITE} for {Fore.BLUE}bash.zip{Fore.WHITE} is {Fore.RED}{bashhash}")
+print(f"{Fore.YELLOW}SHA256{Fore.WHITE} for {Fore.LIGHTBLUE_EX}bat.zip{Fore.WHITE}  is {Fore.RED}{bathash}")
+print(f"{Fore.YELLOW}SHA256{Fore.WHITE} for {Fore.LIGHTBLUE_EX}bash.zip{Fore.WHITE} is {Fore.RED}{bashhash}")
+
+if args.workflow:
+    with ZipFile(f'{archives_dir}/bat.zip', 'r', ZIP_DEFLATED) as archive:
+        archive.extractall(f'{archives_dir}/bat')
+    os.remove(f'{archives_dir}/bat.zip')
+    with ZipFile(f'{archives_dir}/bash.zip', 'r', ZIP_DEFLATED) as archive:
+        archive.extractall(f'{archives_dir}/bash')
+    os.remove(f'{archives_dir}/bash.zip')
